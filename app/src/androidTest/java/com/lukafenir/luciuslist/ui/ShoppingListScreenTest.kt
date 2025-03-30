1eb079dd9d003b24ca9790f0522fb9d0330ebdf4
LukaFenir <laura.cnichols@outlook.com>
2025-03-30 13:14:06 +0100

    feat: added shopping list screen with dummy text

---
+++ app/src/androidTest/java/com/lukafenir/luciuslist/ui/ShoppingListScreenTest.kt
@@ -0,0 +1,28 @@
+package com.lukafenir.luciuslist.ui
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ShoppingListScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun emptyShoppingList_displaysEmptyStateMessage() {
+
+        // When
+        composeTestRule.setContent {
+            ShoppingListScreen()
+        }
+
+        // Then
+        composeTestRule.onNodeWithText("Your shopping list is empty. Tap + to add items.").assertIsDisplayed()
+    }
+}
---
+++ app/src/main/java/com/lukafenir/luciuslist/ui/ShoppingListScreen.kt
@@ -0,0 +1,30 @@
+package com.lukafenir.luciuslist.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lukafenir.luciuslist.ui.theme.LuciuslistTheme
+
+@Composable
+fun ShoppingListScreen(
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Your shopping list is empty. Tap + to add items.")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShoppingListScreenPreview() {
+    LuciuslistTheme {
+        ShoppingListScreen()
+    }
+}
