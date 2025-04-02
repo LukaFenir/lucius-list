9f92cac8d3bab6bec70fe94ba0e25c41866c13b0
LukaFenir <laura.cnichols@outlook.com>
2025-04-02 15:03:08 +0100

    feat: Items can be added to the shopping list screen

--- app/src/test/java/com/lukafenir/luciuslist/model/ShoppingListViewModelTest.kt
+++ app/src/test/java/com/lukafenir/luciuslist/model/ShoppingListViewModelTest.kt
@@ -12,17 +12,20 @@ class ShoppingListViewModelTest {
 
     @Test
     fun `addItem adds an item to the empty view model`(){
+        //given
         var viewModel = ShoppingListViewModel()
 
         //when
         viewModel.addItem("Cheese")
 
+        //then
         assertThat(viewModel.items).hasSize(1)
         assertThat(viewModel.items[0]).isEqualTo(ShoppingItem(1, "Cheese", 1, "", false))
     }
 
     @Test
     fun `addItem multiple times adds multiple items to the view model`(){
+        //given
         var viewModel = ShoppingListViewModel()
 
         //when
@@ -30,6 +33,7 @@ class ShoppingListViewModelTest {
         viewModel.addItem("Banana")
         viewModel.addItem("Whole Wheat")
 
+        //then
         assertThat(viewModel.items).hasSize(3)
         assertThat(viewModel.items[0]).isEqualTo(ShoppingItem(1, "Cheese", 1, "", false))
         assertThat(viewModel.items[1]).isEqualTo(ShoppingItem(2, "Banana", 1, "", false))
